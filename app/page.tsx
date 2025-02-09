122d8bd030a8f57f3db9959a4443df3140a44c74
iAbhayRajput <abhayravirajput@gmail.com>
2025-02-09 18:35:55 +0530

    added timer

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Background from "./components/Background"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import SkullModel from "./components/SkullModel"
+import HauntedCountdown from './components/CountdownTimer'
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true)
@@ -26,7 +27,12 @@ export default function Home() {
             <Background />
             <Header />
             <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-0 md:py-2 lg:py-4 relative">
-              <SkullModel />
+              <div className="relative z-0 h-[60vh] sm:h-[70vh] md:h-[80vh]">
+                <SkullModel />
+              </div>
+              <div className="absolute inset-x-0 top-[40vh] sm:top-[35vh] md:top-[30vh] z-10 pointer-events-none">
+                <HauntedCountdown />
+              </div>
             </main>
             <Footer />
           </motion.div>
