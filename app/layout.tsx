fc325459c463bca72daf631698bcb7d626af3c47
iAbhayRajput <abhayravirajput@gmail.com>
2025-02-16 17:29:14 +0530

    added seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Crimson_Text } from 'next/font/google'
 import './globals.css'
+import { Metadata } from 'next'
 
 const crimsonText = Crimson_Text({ 
   weight: ['400', '600', '700'],
@@ -7,6 +8,27 @@ const crimsonText = Crimson_Text({
   display: 'swap',
   variable: '--font-crimson',
 })
+
+export const metadata: Metadata = {
+  title: 'Prisma 2025 | SRM University Delhi-NCR | Techno-Cultural Fest',
+  description: 'Official website for SRM University Delhi-NCR\'s Prisma 2025 -Techno-cultural fest from February 28 to March 1. Featuring competitions, performances & technical events.',
+  keywords: [
+    'Prisma',
+    'Prisma-SRMUH',
+    'Prisma 2025',
+    'SRM Delhi-NCR fest',
+    'College fest Haryana',
+    'College fest Delhi-NCR',
+    'College fest NCR',
+    'College fest Delhi',
+    'Technical events Sonepat',
+    'Horror theme festival',
+    'SRM University cultural fest'
+  ],
+  openGraph: {
+    images: '/prisma-2025-og.jpg',
+  },
+}
 
 export default function RootLayout({
   children,
